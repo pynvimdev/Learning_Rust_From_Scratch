0de8f2f244422fef1395c3d953470780c4b1e154
Hari <you@example.com>
2021-12-06 13:49:22 +0530

    Basic Rust Dev

--- Learning_Rust/src/main.rs
+++ Learning_Rust/src/main.rs
@@ -1,19 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    let mut gues = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..10);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    // We can also do this in oneline
-    // io::stdin().read_line(&mut gues).expect("Expected Input");
-    io::stdin()
-        .read_line(&mut gues)
-        .expect("Failed to read line");
-    println!("You guessed: {}", guess);
-    println!("You guessed: {}", gues);
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        // io::stdin().read_line(&mut gues).expect("Expected Input");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // Converting guess into a num
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
