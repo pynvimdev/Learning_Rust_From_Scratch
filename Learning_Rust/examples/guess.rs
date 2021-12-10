8a0930478a3790221b95d33c0180912aa1ad03d4
Your Name <you@example.com>
2021-12-10 22:36:09 +0530

    Update rust files

--- Learning_Rust/examples/guess.rs
+++ Learning_Rust/examples/guess.rs
@@ -1,29 +1,25 @@
 use std::io;
 
 fn main() {
-    let (s1 , s2 ) = ("some", "string");
-    let s = [s1 , s2].concat();
-    println!("{}",s);
+    let (s1, s2) = ("some", "string");
+    let s = [s1, s2].concat();
+    println!("{}", s);
 
     let mut age = String::new();
 
-
     println!("Enter your age");
-    io::stdin()
-        .read_line(&mut age);
+    io::stdin().read_line(&mut age);
 
-    let mut age : i32 = match age.trim().parse() {
+    let mut age: i32 = match age.trim().parse() {
         Ok(age) => age,
         Err(_) => -1,
     };
 
     if age < 18 {
         println!("Hello, child");
-    }
-    else if age > 45 && age < 81 {
+    } else if age > 45 && age < 81 {
         println!("Hello Bommer");
-    }
-    else {
+    } else {
         println!("Bye");
     }
 }
