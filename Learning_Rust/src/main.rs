066d29f5e72ee2725bec3b0978bf8aa78fa4b1cd
Hari <you@example.com>
2021-12-10 14:19:09 +0530

    Update Dotfiles

--- Learning_Rust/src/main.rs
+++ Learning_Rust/src/main.rs
@@ -1,7 +1,20 @@
+use std::io;
+use std::cmp;
 use rand::Rng;
 
 fn main() {
-    println!("Hello World");
-    let sec_num = rand::thread_rng().gen_range(1 .. 10);
-    println!("{}",sec_num);
+    println!("Enter Your guess");
+    let sec_num = rand::thread_rng().gen_range(1..10);
+    println!("{}", sec_num);
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Enter Num and ot string");
+
+    let guess : i32 = guess.trim().parse() {
+        Ok(num) -> num,
+        Err(_) -> continue,
+    };
 }
