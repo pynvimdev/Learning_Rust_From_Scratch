d47bfa65500d9297c3dea763dd684561b18b6603
Your Name <you@example.com>
2021-12-11 10:14:05 +0530

    Update

--- Learning_Rust/examples/guess.rs
+++ Learning_Rust/examples/guess.rs
@@ -1,26 +1,5 @@
-use std::io;
-#[allow(unused_must_use)]
-fn main() {
-    let (s1, s2) = ("some", "string");
-    let s = [s1, s2].concat();
-    println!("{}", s);
-
-    let mut age = String::new();
-
-    println!("Enter your age");
-    io::stdin()
-        .read_line(&mut age);
-
-    let age: i32 = match age.trim().parse() {
-        Ok(age) => age,
-        Err(_) => -1,
-    };
-
-    if age < 18 {
-        println!("Hello, child");
-    } else if age > 45 && age < 81 {
-        println!("Hello Bommer");
-    } else {
-        println!("Bye");
+fn main(){
+    for i in 1..100 {
+        println!("{} Current value is ", i);
     }
 }
